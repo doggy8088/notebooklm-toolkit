7e4a04f651ef0517a1e72eb3b620d3b2c020b8e1
Will <doggy.huang@gmail.com>
2025-08-01 21:18:13 +0100

    feat: 更新 sidePanel 的程式邏輯以支持動態顯示與隱藏

--- background/service-worker.js
+++ background/service-worker.js
@@ -1,8 +1,5 @@
-const DEFAULT_URL = "https://notebooklm.google.com/";
-
-chrome.action.onClicked.addListener((tab) => {
-  chrome.sidePanel.open({ tabId: tab.id });
-});
+const DEFAULT_URL = "https://notebooklm.google.com";
+const SIDEPANEL_PATH = "sidepanel/sidepanel.html";
 
 chrome.runtime.onInstalled.addListener(() => {
   // Create context menu
@@ -12,15 +9,53 @@ chrome.runtime.onInstalled.addListener(() => {
     contexts: ["action"]
   });
 
+  // 一定要將 openPanelOnActionClick 設定為 true，才能在點擊擴充功能圖示時打開側邊欄
+  chrome.sidePanel
+    .setPanelBehavior({ openPanelOnActionClick: true })
+    .catch((error) => console.error(error));
+
+  // 一定要將 sidePanel 的 enabled 設為 false，才能在點擊擴充功能圖示時預設隱藏側邊欄
+  chrome.sidePanel
+    .setOptions({ path: SIDEPANEL_PATH, enabled: false });
+
+  // Notification
   chrome.notifications.onClicked.addListener(function (notificationId) {
     chrome.tabs.create({ url: DEFAULT_URL });
   });
+});
+
+chrome.action.onClicked.addListener(async (tab) => {
+  if (tab.url.startsWith(DEFAULT_URL)) {
+    chrome.sidePanel.setOptions({ enabled: true });
+  } else {
+    chrome.tabs.create({ url: DEFAULT_URL });
+  }
 });
 
 // Handle context menu clicks
 chrome.contextMenus.onClicked.addListener((info, tab) => {
   if (info.menuItemId === "openCustomPrompts") {
     chrome.sidePanel.open({ tabId: tab.id });
+  }
+});
+
+// Handle tab updates - close sidePanel when navigating to a different URL
+chrome.tabs.onUpdated.addListener(async (tabId, changeInfo, tab) => {
+  if (!tab.url) return;
+
+  if (tab.url.startsWith(DEFAULT_URL)) {
+    await chrome.sidePanel.setOptions({
+      tabId,
+      path: SIDEPANEL_PATH,
+      enabled: true
+    });
+  } else {
+    // Disables the side panel on all other sites
+    await chrome.sidePanel.setOptions({
+      tabId,
+      path: SIDEPANEL_PATH,
+      enabled: false
+    });
   }
 });
 
